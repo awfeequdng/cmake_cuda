27c22bf8d5d59e0708f1dde38755d508367a21ac
caipengxiang <291458254@qq.com>
2022-05-14 13:21:35 +0800

    add matmul_kernel function

--- matmul_cuda.cu
+++ matmul_cuda.cu
@@ -1,10 +1,26 @@
 #include "matmul.hh"
 
+
+// Matrix multiplication kernel called by MatMul()
+ __global__ void matmul_kernel(Matrix A, Matrix B, Matrix C) {
+    C.element[0] = A.element[0] + B.emement[0];
+ }
+
 void matmul(Matrix& A, Matrix& B, Matrix& C) {
-    Matrix d_A;
+    Matrix d_A, d_B, d_C;
     auto size = A.height * A.weight * sizeof(double);
     cudaMalloc(&d_A.element, size);
     cudaMemcpy(d_A.element, A.element, size, cudaMemcpyHostToDevice);
 
+    size = B.height * B.weight * sizeof(double);
+    cudaMalloc(&d_B.element, size);
+    cudaMemcpy(d_B.element, B.element, size, cudaMemcpyHostToDevice);
+
+    matmul_kernel<<<1, 1>>>(d_A, d_B, d_C);
+
+    __syncthreads();
+
     cudaFree(d_A.element);
+    cudaFree(d_B.element);
+    cudaFree(d_C.element);
 }
